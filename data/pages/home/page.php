09035085cbd0cfed190fbc454d5267d5257b80c4
Toshib-htr <keithbunnies@gmail.com>
2017-01-06 23:37:45 -0500

    Wrong message

--- data/pages/home/page.php
+++
@@ -1,3 +0,0 @@
-<h1 style="text-align: center;">You can change this page and so much more in the settings panel!</h1>
-<p style="text-align: center;">Click "Admin Panel" at the bottom of the page!</p>
-<p style="text-align: center;">&nbsp;</p>
