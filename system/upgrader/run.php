7fb223dcd1d46f3c851b23761b571e503fe51071
Toshi Bennet <toshi@gardenwolf.com>
2017-01-22 19:38:53 -0500

    Validates the current session

--- system/upgrader/run.php
+++ system/upgrader/run.php
@@ -6,10 +6,25 @@
 		die('403 FORBIDDEN: You are not allowed to access that file outside its normal running location.');
 	}
 	//////////UPGRADE THINGS GO HERE IF CONFIGS NEED CHANGING///////////////
+	// Starts here{
 	
+	require('./data/colours.php');
+	$data="<?php\n\$theme = array(";
 	
-	//code goes here...
+	foreach($themeColours as $item => $value){
+		$data = $data."'".$item."' => '".$value."',\n";
+	}
 	
+	$data = $data.");";
+	
+	$file = fopen('./data/theme.php','w');
+	fwrite($file,$data);
+	fclose($file);
+	
+	unlink("./data/colours.php");
+	
+	// end}
+	//dont change this. It is required.
 	$dataVersionFile = fopen('./data/dataVersion.php', 'w');
 	fwrite($dataVersionFile, "<?php\n\$dataVersion=".$TB['version'].";\n?>");
 	fclose($dataVersionFile);
