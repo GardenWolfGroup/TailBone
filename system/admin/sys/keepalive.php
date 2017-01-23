ac4a94339fa9702b12dd678d9f4f83c1661af603
Toshi Bennet <toshi@gardenwolf.com>
2017-01-22 19:28:53 -0500

    The php script that checks if the session is alive

--- system/admin/sys/keepalive.php
+++ system/admin/sys/keepalive.php
@@ -4,29 +4,7 @@
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
 	
-	session_start();
-	//looks to see the last activity time.
-	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
-		session_unset();
-		session_destroy();
-	}
-	
-	//makes sure the user still exists in the "database"
-	if(!isset($users[$_SESSION['user']])){
-		echo('false');
-		die();
-	}
-	
-	if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == 'true'){
-		if(isset($_SESSION['keepAlive'])){
-			//setting the number of times the session was kept alive.
-			$_SESSION['keepAlive'] += 1;
-		}else{
-			$_SESSION['keepAlive'] = 1;
-		}
-		$_SESSION['loggedin'] = "true";
-		//updates the last activity time.
-		$_SESSION['LAST_ACTIVITY'] = time();
+	if($loggedin){
 		echo('true');
 		die();
 	}else{
