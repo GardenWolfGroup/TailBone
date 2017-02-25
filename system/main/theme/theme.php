ed6f51b039d33a44b51f8e3d6d3b8b8df665f527
Keith Va <keithbunnies@gmail.com>
2017-02-24 22:51:41 -0500

    The theme file, duh

---
+++ system/main/theme/theme.php
@@ -0,0 +1,61 @@
+<?PHP
+	require('./data/theme.php');
+	if($theme['customFont'] != ""){
+		$themeFont = '"'.$theme['customFont'].'",'.$theme['fontFamily'];
+	}else{
+		$themeFont = $theme['fontFamily'];
+	}
+	echo('
+	<meta name="theme-color" content="'.$theme['navBackground'].'">
+<style>
+	'.file_get_contents('./data/custom.css').'
+	*{
+		font-family:'.$themeFont.';
+	}
+	body{
+		background:'.$theme["bodyBackground"].'!important;
+		background-image:url('.$theme["bodyBackgroundImage"].')!important;
+		background-repeat:'.$theme["bodyBackgroundRepeat"].'!important;
+	}
+	#content{
+		background:'.$theme["contentBackground"].'!important;
+		color:'.$theme["contentText"].'!important;
+	}
+	hr{
+		border-color:white!important;
+	}
+	#topper{
+		background:'.$theme["topperBackground"].'!important;
+		color:'.$theme["topperText"].'!important;
+	}
+	#ender{
+		background:'.$theme["enderBackground"].'!important;
+	}
+	#nav{
+		background:'.$theme["navBackground"].'!important;
+		border-bottom:2px solid '.$theme["navBackground"].';
+	}
+	#nav a div{
+		color:'.$theme["navText"].'!important;
+	}
+	.nav_'.$pageName.'{
+		background-color:'.$theme["navHighlightColour"].'!important;
+	}
+	#nav-navigation div:hover{
+		background-color:'.$theme["navHighlightColour"].';	
+	}
+	#nav-navigation div:active{
+		background-color:'.$theme["navHighlightColour"].';
+	}
+	#editButton{
+		background-color:'.$theme['topperBackground'].';
+	}
+	#editButtonError{
+		background-color:'.$theme['topperBackground'].';
+	}
+	.fancy_input, .abtn_blue{
+		background-color:'.$theme['bodyBackground'].';
+	}
+</style>
+	');
+?>
