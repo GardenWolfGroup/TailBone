5e53ba02e88944b272f29c2f3c3895e9dd870755
Keith Va <keithbunnies@gmail.com>
2017-02-24 22:42:49 -0500

    Keeps people out of your filesystem

---
+++ system/index.php
@@ -0,0 +1,11 @@
+<?php
+	session_start();
+	if(!isset($dirLock)){
+		$dirLock = '../';
+	}
+	
+	$_SESSION['MSGBanner'] = '403, You are not allowed to be there!';
+	$_SESSION['MSGType'] = 2;
+	
+	header('Location: '.$dirLock.'?admin');
+?>
