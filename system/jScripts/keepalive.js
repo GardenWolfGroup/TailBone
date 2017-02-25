3730f1b554470eed66d9478146fd4616fb3ce83f
Keith Va <keithbunnies@gmail.com>
2017-02-24 22:48:27 -0500

    Checks to make sure that the current session is valid

---
+++ system/jScripts/keepalive.js
@@ -0,0 +1,18 @@
+consoleVars = 'background-color:black;color:#429a86;';
+function checkLoggedin(data){
+	console.log("%c Checking keepalive.",consoleVars);
+	if(data != "true"){
+		location.href="./";
+	}else{
+		console.log("%c Session is still alive.",consoleVars);
+	}
+}
+document.write('<iframe src="./index.php?admin&request&action=keepalive" onload="checkLoggedin(this.contentDocument.body.innerHTML);" id="keepSessionAlive" style="display:none;"></iframe>');
+window.setInterval(function(){
+  keepAlive();
+}, 60000);
+							
+function keepAlive() {
+	console.log("%c Keeping session alive.",consoleVars);
+	document.getElementById("keepSessionAlive").src = "./index.php?admin&request&action=keepalive";
+}
